77068dabc0941676b76847e28df68c7725488454
Luke Berger <Luke_Berger@aol.de>
2026-07-03 16:06:38 +0200

    Encoded URL payload in Base64 to secure experiment parameters: Makes taskId and surveyId non-human-readable to prevent participants from switching tasks or altering IDs out of curiosity. Added fallback logic for local testing

--- src/hooks/useUrlParams.ts
+++ src/hooks/useUrlParams.ts
@@ -1,8 +1,36 @@
 export const useUrlParams = () => {
   const queryParams = new URLSearchParams(window.location.search);
 
-  const taskId = queryParams.get('taskId') === '2' ? 2 : 1;
-  const surveyId = queryParams.get('surveyId') || 'local-test-user-001';
+  // searching for p-parameter (payload)
+  const payload = queryParams.get('p');
+
+  // fallback if nothing is attached to the URL
+  let taskId = 1;
+  let surveyId = 'local-test-user-001';
+
+  if (payload) {
+    try {
+      // translate base64 string into text
+      const decodedString = atob(payload);
+
+      // tranform text into JS-object
+      const parsedData = JSON.parse(decodedString);
+
+      // read the data
+      if (parsedData.taskId === 2) taskId = 2;
+      if (parsedData.surveyId) surveyId = parsedData.surveyId;
+    } catch (error) {
+      // if someone tries to write or change the p-parameter it trows an error
+      console.warn('Error: ' + error);
+    }
+  } else {
+    // ONLY FOR TESTING PURPOSES, remove after implentation phase is over
+    const plainTaskId = queryParams.get('taskId');
+    const plainSurveyId = queryParams.get('surveyId');
+
+    if (plainTaskId === '2') taskId = 2;
+    if (plainSurveyId) surveyId = plainSurveyId;
+  }
 
   return { taskId, surveyId };
 };
